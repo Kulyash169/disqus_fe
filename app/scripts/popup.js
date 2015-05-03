6572100b83ef4ba051ee025dfdc9f9044c7627b0
Kulyash <kulyash169@gmail.com>
2015-05-03 20:58:52 +0600

    doing backend: put and get query

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,27 +1,47 @@
 var check=0;
-var count=0;
-(function() {
-	
-	$('.col-lg-10 > input').keyup(function() {
-		var empty = false;
-		$('.col-lg-10 > input').each(function() {
-			if ($(this).val() == '') {
-			empty = true;
-			}
-		});
 
-		if (empty) {
-			$('#focusedInput').attr('disabled', 'disabled');
-		} else {
-			$('#focusedInput').removeAttr('disabled');
+(function() {
+		// get email and name from local storage
+		var mainEmail = localStorage.getItem("email");
+		var mainName = localStorage.getItem("name");
+		var empty = false;
+
+		// check 2 inputs (if is empty, then fill it with value from local storage), else it will be empty
+		if(mainEmail!=null || mainName!=null){
+			var emailInput = document.getElementById("inputEmail");
+			emailInput.value=mainEmail;
+			var nameInput = document.getElementById("textArea");
+			nameInput.value=mainName;
+			empty = false;
 		}
+		// check for NULL when pressed on email or name
+		$('.col-lg-10 > input').keyup(function() {
+			empty = false;
+			$('.col-lg-10 > input').each(function() {
+				if ($(this).val() == '') {
+					empty = true;
+				}
+			});
+			if (empty) {
+				$('#focusedInput').attr('disabled', 'disabled');
+			} else {
+				$('#focusedInput').removeAttr('disabled');
+			}
+
 		});
+		if (empty) {
+				$('#focusedInput').attr('disabled', 'disabled');
+			} else {
+				$('#focusedInput').removeAttr('disabled');
+			}
+
 
 		$('#focusedInput').keyup(function() {
 			var empty = false;
 			if ($(this).val() == '') {
 				empty = true;
 			}
+
 			if (empty) {
 				document.getElementById("focusedInput").className = "form-control empty";
 			} else {
@@ -29,72 +49,109 @@ var count=0;
 			}
 		});
 
-		$('#focusedInput').keyup(function(event) {
-	    	if(event.keyCode == 13){
-	    		var list = document.getElementById("listID");
-	    		if(check!=0){
-	    			var separator = document.createElement("div");
-	    			separator.setAttribute("class","list-group-separator");
-	    			list.appendChild(separator);
-	    		}
-	    		//CREATE NE ELEMENT (ITEM)
-	    		var item = document.createElement("div");
-	    		item.setAttribute("class","list-group-item");
-	    		list.appendChild(item);
 
-	    			// Icon of comment 
-		    		var icon = document.createElement("div");
-		    		icon.setAttribute("class","row-action-primary");
-		    		item.appendChild(icon);
+		//set GET query
+		$.ajax({url: 'http://127.0.0.1:8000/api/v1/comment/', 
+			success: function(result){
+			//get all comments
+				for (var i=0;i<result.objects.length;i++){
 
-			    		var pic = document.createElement("i");
-			    		pic.setAttribute("class","mdi-file-folder");
-			    		icon.appendChild(pic);
-
-			    	// Content of comment 
-		    		var content = document.createElement("div");
-		    		content.setAttribute("class","row-content");
-		    		item.appendChild(content);
-
-		    			//Create a current date
-						var today = new Date();
-						var dd = today.getDate();
-						var mm = today.getMonth()+1; 
-						var yyyy = today.getFullYear();
-
-						if(dd<10) {
-						dd='0'+dd
-						} 
-
-						if(mm<10) {
-						mm='0'+mm
-						} 
-
-						today = dd+'.'+mm+'.'+yyyy;
-
-						//Set a current date
-			    		var time = document.createElement("div");
-			    		time.setAttribute("class","least-content");
-			    		time.appendChild(document.createTextNode(today));
-			    		content.appendChild(time);
-
-			    		//Set a user name
-			    		var header = document.createElement("h4");
-			    		header.setAttribute("class","list-group-item-heading");
-			    		header.appendChild(document.createTextNode(document.getElementById("textArea").value));
-			    		content.appendChild(header);
-
-			    		//Set a comment
-			    		var comment = document.createElement("p");
-			    		comment.setAttribute("class","list-group-item-text");
-			    		comment.appendChild(document.createTextNode(document.getElementById("focusedInput").value));
-			    		content.appendChild(comment);
+					var list = document.getElementById("listID");
+			    		if(check!=0){
+			    			var separator = document.createElement("div");
+			    			separator.setAttribute("class","list-group-separator");
+			    			list.appendChild(separator);
+			    		}
 			    		
-			    		check=1;
-			    		count++;
-			
+			    		//CREATE NEW ELEMENT (ITEM)
+			    		var item = document.createElement("div");
+			    		item.setAttribute("class","list-group-item");
+			    		list.appendChild(item);
 
-	    	}
+			    			// Icon of comment 
+				    		var icon = document.createElement("div");
+				    		icon.setAttribute("class","row-action-primary");
+				    		item.appendChild(icon);
+
+				    			// get picture from Gravatar
+				    			var q = document.getElementById('inputEmail').value;
+					    		var pic = document.createElement("img");
+					    		gravatar = result.objects[i].image;
+					    		pic.setAttribute("class","circle");
+					    		pic.setAttribute("src",gravatar);
+					    		icon.appendChild(pic);
+
+					    	// Content of comment 
+				    		var content = document.createElement("div");
+				    		content.setAttribute("class","row-content");
+				    		item.appendChild(content);
+
+				    			//Create a current date
+								var day = result.objects[i].pub_time;
+								var today = day.substring(0,10);
+								var dd = today.substring(8,10);
+								var mm = today.substring(5,7);
+								var yyyy = today.substring(2,4);
+
+								//Set a current date
+					    		var time = document.createElement("div");
+					    		time.setAttribute("class","least-content");
+					    		time.appendChild(document.createTextNode(dd+'.'+mm+'.'+yyyy));
+					    		content.appendChild(time);
+
+					    		//Set a user name
+					    		var header = document.createElement("h4");
+					    		header.setAttribute("class","list-group-item-heading");
+					    		header.appendChild(document.createTextNode(result.objects[i].author_title));
+					    		content.appendChild(header);
+
+					    		//Set a comment
+					    		var comment = document.createElement("p");
+					    		comment.setAttribute("class","list-group-item-text");
+					    		comment.appendChild(document.createTextNode(result.objects[i].text));
+					    		content.appendChild(comment);
+					    		
+					    		check=1;
+				}
+			}
+		});
+
+
+		$('#focusedInput').keyup(function(event) {
+
+
+	    	if(event.keyCode == 13){
+	    		
+	    		//put mail and name to localStorage
+	    		var email=document.getElementById("inputEmail").value;
+				localStorage.setItem("email",email);
+				var name=document.getElementById("textArea").value;
+				localStorage.setItem("name",name);
+
+				//set POST query 
+				var pic = document.getElementById("inputEmail");
+				var name = document.getElementById("textArea");
+
+				var data1 = JSON.stringify(
+					{
+						"text": document.getElementById('focusedInput').value,
+						"author_title": name.value,
+						"image":'http://www.gravatar.com/avatar/'+CryptoJS.MD5(pic.value)
+					}
+				);
+
+				$.ajax({
+				url: 'http://127.0.0.1:8000/api/v1/comment/',
+				type: 'POST',
+				contentType: 'application/json',
+				data: data1,
+				dataType: 'json',
+				processData: false
+				});
+
+			}
+
+	    	
     	});
 
 
